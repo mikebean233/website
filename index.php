f0cacc4303c449e52a892971e87b28e8a6a6c73a
Michael Peterson <mikebean233@yahoo.com>
2016-03-07 19:59:28 -0800

    Access Log

--- index.php
+++ index.php
@@ -1,9 +1,18 @@
 <html>
 <head>
+	<script src="jquery.js"></script>
 </head>
 <body>
+<script language="javascript">
+$( document ).ready(function() {
+    $("#accessTable").DataTable("/accesslog.log");
+}
+</script>
+});
 <link rel="shortcut icon" href="http://www.mikeswebserver.com/favicon.ico" />
 <a href="http://www.github.com/mikebean233">github</a><br>
 <a href="prob.html">Probability</a>
-</body>
+<label>Access Log</label>
+<div id="accessTable"></div>
+</body>d
 </html>
