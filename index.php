c8636eaba13a90fe9236cd864705ab78a40ba9b7
Michael Peterson <mikebean233@yahoo.com>
2016-03-20 00:03:25 -0700

    added main menu, made content area more modular.

--- index.php
+++ index.php
@@ -1,100 +1,107 @@
 <!DOCTYPE html>
 <html lang="en">
-    <title>
-        Michael Peterson
-    </title>
-    <head>
-        <link rel="stylesheet" type="text/css" href="css/jquery.dataTables.css"/>
-        <link rel="stylesheet" type="text/css" href="css/buttons.dataTables.css"/>
-        <link rel="stylesheet" type="text/css" href="css/colReorder.dataTables.css"/>
-        <link rel="stylesheet" type="text/css" href="css/keyTable.dataTables.css"/>
-        <link rel="stylesheet" type="text/css" href="css/responsive.dataTables.css"/>
-        <link rel="stylesheet" type="text/css" href="css/rowReorder.dataTables.css"/>
-        <link rel="stylesheet" type="text/css" href="css/jquery-ui.css"/>
-        <link rel="stylesheet" type="text/css" href="css/jquery-ui.structure.css"/>
-        <link rel="stylesheet" type="text/css" href="css/jquery-ui.theme.css"/>
-        <link rel="stylesheet" type="text/css" href="css/bootstrap.css"/>
-        <link rel="stylesheet" type="text/css" href="css/bootstrap-theme.css"/>
+<title>
+    Michael Peterson
+</title>
+<head>
+    <link rel="stylesheet" type="text/css" href="css/jquery.dataTables.css"/>
+    <link rel="stylesheet" type="text/css" href="css/buttons.dataTables.css"/>
+    <link rel="stylesheet" type="text/css" href="css/colReorder.dataTables.css"/>
+    <link rel="stylesheet" type="text/css" href="css/keyTable.dataTables.css"/>
+    <link rel="stylesheet" type="text/css" href="css/responsive.dataTables.css"/>
+    <link rel="stylesheet" type="text/css" href="css/rowReorder.dataTables.css"/>
+    <link rel="stylesheet" type="text/css" href="css/jquery-ui.css"/>
+    <link rel="stylesheet" type="text/css" href="css/jquery-ui.structure.css"/>
+    <link rel="stylesheet" type="text/css" href="css/jquery-ui.theme.css"/>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css"/>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap-theme.css"/>
 
 
-        <script type="text/javascript" src="js/jquery-2.2.0.js"></script>
-        <script type="text/javascript" src="js/jquery.dataTables.js"></script>
-        <script type="text/javascript" src="js/dataTables.buttons.js"></script>
-        <script type="text/javascript" src="js/dataTables.colReorder.js"></script>
-        <script type="text/javascript" src="js/dataTables.keyTable.js"></script>
-        <script type="text/javascript" src="js/dataTables.responsive.js"></script>
-        <script type="text/javascript" src="js/jquery-ui.js"></script>
-        <script type="text/javascript" src="js/bootstrap.js"></script>
-    </head>
+    <script type="text/javascript" src="js/jquery-2.2.0.js"></script>
+    <script type="text/javascript" src="js/jquery.dataTables.js"></script>
+    <script type="text/javascript" src="js/dataTables.buttons.js"></script>
+    <script type="text/javascript" src="js/dataTables.colReorder.js"></script>
+    <script type="text/javascript" src="js/dataTables.keyTable.js"></script>
+    <script type="text/javascript" src="js/dataTables.responsive.js"></script>
+    <script type="text/javascript" src="js/jquery-ui.js"></script>
+    <script type="text/javascript" src="js/bootstrap.js"></script>
+</head>
+<style>
+    .nav a {
+        font-size: 20px
+    }
+    .dropdown
+    .dropdown-menu li:hover .sub-menu {
+        visibility: visible;
+    }
 
-    <script language="javascript">
-    $( document ).ready(function() {
-        var table;
-        var accessTableColumns = [
-            {title: "time"},
-            {title: "host"},
-            {title: "user : port"},
-            {title: "remote logname"},
-            {title: "remote user"},
-            {title: "first line of request"},
-            {title: "status"},
-            {title: "referer"},
-            {title: "user agent"},
-            {title: "bytes received"},
-            {title: "bytes sent"}
-        ];
+    .dropdown:hover .dropdown-menu {
+        display: block;
+    }
 
-    $.ajax({
-            url: "/access.log",
-            type: "POST",
-            dataType: "text",
-            contentType: "application/octet-stream",
-            success: function(json){
-                var splitByNewline = json.split("\n");
-                var rowData = [];
-                for(var i = 0; i < splitByNewline.length; ++i){
-                    var thisLineTokens = splitByNewline[i].split("<delimiter>");
-                    if(thisLineTokens && thisLineTokens.length >= accessTableColumns.length)
-                        rowData[i] = thisLineTokens;
-                }
-                table = $("#accessTable").DataTable({
-                data: rowData,
-                    columns: accessTableColumns,
-                    scrollY: "10em",
-                    scrollX: true
-                });
-            }
+</style>
+<script language="javascript">
+    var menuItemClickHandler = function(e){
+        $("#mainContainer").empty().load(e.target.id + ".html", function(){
+            initPartial()
         });
+    };
+
+    $(document).ready(function(){
+
+        $("#mainContainer").empty().load("home.html", function(){
+            initPartial()
+            });
+        $(".mainMenuItem").click(menuItemClickHandler);
     });
-    </script>
-    <body class="container-fluid" style="background-color: aliceblue">
+</script>
+
+
+<body class="container-fluid" style="background-color: aliceblue">
         <div class="row">
             <div class="col-lg-12">
-                <h1 class="text-center">mikeswebserver<small>.com</small></h1>
+                <h4 class="mainMenuItem text-left">
+                    <a href=""style="text-decoration: none; color: inherit">
+                        mikeswebserver<small>.com</small>
+                    </a>
+                </h4>
             </div>
         </div>
         <div class="row">
-            <div class="col-sm-2">
-                <link rel="shortcut icon" href="http://www.mikeswebserver.com/favicon.ico" />
-                <ul style="list-style-type: none; padding-left: 0">
-                    <li><a href="http://www.github.com/mikebean233">Github</a></li>
-                    <li><a href="prob.html">Probability</a></li>
-                    <li><a href="https://www.linkedin.com/in/mikebean233">LinkedIn</a></li>
+            <div class="col-lg-12">
+                <ul class="nav nav-pills navbar-default collapse navbar-collapse">
+
+                    <li><a href="#" class="mainMenuItem" id="home">Home</a></li>
+                    <li><a href="#" class="mainMenuItem" id="tools">Tools</a></li>
+
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle mainMenuItem" id="projects" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                            Projects<span class="caret"></span>
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a href="#" class="mainMenuItem clickable" id="missileCommand">Missile Command Clone (C#, WPF)</a></li>
+                            <li><a href="#" class="mainMenuItem" id="blackjack">Blackjack Library (C)</a></li>
+                            <li><a href="#" class="mainMenuItem" id="mediaPlayer">Media Player (C#, WPF, SqlLite)</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="#" class="mainMenuItem" id="contact">Contact</a></li>
+
+
+                    <li class="dropdown navbar-right">
+                        <a href="#" class="dropdown-toggle mainMenuItem"  style="font-size: 1em" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                            Admin<span class="caret"></span>
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a href="#" class="mainMenuItem" id="access"  style="font-size: 1em">Server Activity</a></li>
+                        </ul>
+                    </li>
                 </ul>
             </div>
-            <div class="col-sm-10">
-                    <h3>Access Log</h3>
-                    <p>
-                        <div id="accessTableWrapper" class="panel panel-default" style="background-color: mintcream">
-                            <table id="accessTable" class="panel-body stripe row-border cell-border order-column nowrap" style="font-size: .7em">
-                                <thead>
-                                    <tbody>
-                                    </tbody>
-                                </thead>
-                            </table>
-                        </div>
-                    </p>
-            </div>
         </div>
-    </body>
+    <div class="row">
+        <div class="col-sm-12">
+            <div id="mainContainer"></div>
+        </div>
+    </div>
+</body>
 </html>
